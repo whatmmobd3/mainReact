e3f8d6ce99931c67fb481c9f2946a2a090aeb13b
David Nguyen <ddnn2026@Davids-MacBook-Pro.local>
2021-03-08 20:49:05 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header/Header";
+import ImageGrid from './components/ImageGrid/ImageGrid'
 
 class App extends React.Component {
-    render() {
-        return (
-            <h1>as</h1>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <ImageGrid />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import './styles.css';
+
+const Header = () => <header>What the JavaScript</header>;
+
+export default Header;
