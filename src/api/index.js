de8bdc7c1ddf221c4f7f8174dcce73837a94a703
David <>
2021-03-09 15:02:41 +0700

    add images

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const KEY = "5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02";
 const URL = `https://api.unsplash.com/photos/?client_id=`;
 
 const fetchImages = async (page) => {
-  const response = await fetch(`${URL}${KEY}&per_page=${page}`);
+  const response = await fetch(`${URL}${KEY}&per_page=3&page=${page}`);
   const data = await response.json();
 
   if (response.state > 400) {
--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -2,6 +2,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loadImages } from "../../actions";
+
+import Button from '../Button/Button'
+
 import "./styles.css";
 
 class ImageGrid extends Component {
@@ -10,7 +13,7 @@ class ImageGrid extends Component {
   }
 
   render() {
-    const { images, error } = this.props;
+    const { images, error, isLoading,loadImages } = this.props;
     return (
       <div className="content">
         <section className="grid">
@@ -22,9 +25,14 @@ class ImageGrid extends Component {
               <img src={image.urls.small} alt={image.user.username} />
             </div>
           ))}
-          <a onClick={this.props.loadImages}>Load Images</a>
         </section>
         {error && <div className="error">{JSON.stringify(error)}</div>}
+        <Button
+        onClick={() => !isLoading && loadImages()}
+        loading={isLoading}
+        >
+          Load More
+        </Button>
       </div>
     );
   }
